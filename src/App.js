3e58a1e1ce3c77da44a4a401f88f295cb4b10b20
hyeleeeeen <heo22969087@gmail.com>
2022-12-16 17:14:57 +0900

    :sparkle: feat: 라우트 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './pages/Main';
+import Gallery from './pages/Gallery';
+import Play from './pages/Play';
+import Letter from './pages/Letter';
+import Cam from './pages/Cam';
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/happy" element={<Gallery />} />
+        <Route path="/birthday" element={<Play />} />
+        <Route path="/to" element={<Letter />} />
+        <Route path="/you" element={<Cam />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
