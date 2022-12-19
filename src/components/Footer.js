6780c9df83c4a29edc416186ab3ebc27f2e7b9da
hyeleeeeen <heo22969087@gmail.com>
2022-12-19 13:34:15 +0900

    풋터 구현

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,21 @@
 const Footer = () => {
-  return <p>hi</p>;
+  return (
+    <div className="bg-red-500 text-[#8F8989] flex justify-evenly xl:justify-around h-48 place-items-center">
+      <div>
+        <span>Gift for</span>{' '}
+        <a href="https://instagram.com/chaehyunsta?igshid=YTY2NzY3YTc=">
+          {' '}
+          @chaehyunsta
+        </a>
+      </div>
+      <div>
+        <span>Creator </span>
+        <a href="https://instagram.com/hyeleeeeen?igshid=YTY2NzY3YTc=">
+          @hyeleeeeen
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
