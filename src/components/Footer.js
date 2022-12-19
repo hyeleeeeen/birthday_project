fd0f5f5d5da7717f327c1030992d3d450570c9c5
hyeleeeeen <heo22969087@gmail.com>
2022-12-19 17:41:26 +0900

    :art:style : css 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-red-500 text-[#8F8989] flex justify-evenly xl:justify-around h-48 place-items-center">
+    <div className="bg-red-500 text-[#8F8989] flex justify-between xl:justify-between h-48 place-items-center">
       <div>
         <span>Gift for</span>{' '}
         <a href="https://instagram.com/chaehyunsta?igshid=YTY2NzY3YTc=">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import HeaderGif from '../asset/images/HeaderGif.gif';
+import headerGif from '../asset/images/headerGif.gif';
 
-const Header = ({ Link }) => {
+const Header = () => {
   return (
     <>
-      <div className=" w-full xl:bg-yellow-500 lg:bg-blue-400 md:bg-black max-md:bg-pink-500">
+      <div className=" w-full xl:bg-yellow-500 lg:bg-blue-400 md:bg-black max-md:bg-pink-500 ">
         <a href="/" className="flex justify-center">
           <img
-            src={HeaderGif}
+            src={headerGif}
             alt="cake"
             className="xl:w-1/4 lg:w-1/3 md:w-1/3 max-md:w-1/2 "
           />
