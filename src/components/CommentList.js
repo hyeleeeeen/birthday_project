20f32f00e7fd44b5b292c53388b8a8f9e23231a6
hyeleeeeen <heo22969087@gmail.com>
2022-12-19 23:25:04 +0900

    :sparkle: feat : map 추가

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,14 +1,17 @@
-const CommentList = () => {
+const CommentList = ({ commentData }) => {
   return (
-    <section className="bg-pink-500 ">
-      <div className="flex flex-col border-green-600">
-        <span className="text-left">생일축하해</span>
-        <span className="text-right">from 둘리</span>
-      </div>
-      <div className="flex flex-col border-green-600">
-        <span className="text-left">생일축하해</span>
-        <span className="text-right">from 둘리</span>
-      </div>
+    <section className="bg-[#FAF8ED] p-10 rounded-[4rem]">
+      {commentData.map((item) => {
+        return (
+          <div
+            key={item.id}
+            className="flex flex-col w-4/5 m-auto mt-4 border border-[#D9D9D9] bg-white rounded-full"
+          >
+            <span className="text-left ml-4 mt-2">{item.content}</span>
+            <span className="text-right mr-4 mb-2">{item.nickName}</span>
+          </div>
+        );
+      })}
     </section>
   );
 };
