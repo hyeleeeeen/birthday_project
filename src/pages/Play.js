d66e530e27615ab6da041d075932edbfc588a93b
hyeleeeeen <heo22969087@gmail.com>
2022-12-19 17:42:47 +0900

    :sparkle: feat: birthday 페이지 레이아웃 작성

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,5 +1,15 @@
+import ciao from '../asset/images/ciao.gif';
+
 const Play = () => {
-  return;
+  return (
+    <main>
+      <img
+        src={ciao}
+        alt="congratulation"
+        className="m-auto xl:w-1/3 lg:w-1/2 md:w-1/2 max-md:w-3/4"
+      />
+    </main>
+  );
 };
 
 export default Play;
