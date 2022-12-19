5e82a15189c5e80e8c1e8abaf92ffc42767006db
hyeleeeeen <heo22969087@gmail.com>
2022-12-19 13:34:45 +0900

    홈 화면 구현

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -3,8 +3,13 @@ import mainpic from '../asset/images/mainpic.jpeg';
 const MainView = () => {
   return (
     <>
-      <div className="bg-black-500 w-50"></div>
-      {/* <img src={mainpic} alt="pic" /> */}
+      <div className="flex flex-col font-bold text-2xl text-gray-500 mt-32 mb-32 ">
+        <img
+          src={mainpic}
+          alt="pic"
+          className="xl:w-1/3 lg:w-1/2 md:w-1/2 max-md:w-3/4 m-auto"
+        />
+      </div>
     </>
   );
 };
