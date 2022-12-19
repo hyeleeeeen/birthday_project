e5662f8b82d66c13593c49b6f2ff6677aa9c6be0
hyeleeeeen <heo22969087@gmail.com>
2022-12-19 13:33:53 +0900

    :sparkle: feat : 헤더 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
-const Header = () => {
-  return <p>hello</p>;
+import HeaderGif from '../asset/images/HeaderGif.gif';
+
+const Header = ({ Link }) => {
+  return (
+    <>
+      <div className=" w-full xl:bg-yellow-500 lg:bg-blue-400 md:bg-black max-md:bg-pink-500">
+        <a href="/" className="flex justify-center">
+          <img
+            src={HeaderGif}
+            alt="cake"
+            className="xl:w-1/4 lg:w-1/3 md:w-1/3 max-md:w-1/2 "
+          />
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default Header;
