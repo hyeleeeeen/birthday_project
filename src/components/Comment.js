81c34a7cc81c6f1b72f6d6cbafe27168880ca18a
hyeleeeeen <heo22969087@gmail.com>
2022-12-19 23:23:27 +0900

    :sparkle: feat: 더미데이터 생성

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,10 +2,19 @@ import CommentInput from './CommentInput';
 import CommentList from './CommentList';
 
 const Comment = () => {
+  const commentData = [
+    { id: 1, nickName: '둘리', content: '생일축하해!' },
+    {
+      id: 2,
+      nickName: '냥아치',
+      content:
+        '올 생일이냠ㅋ 진짜루 생일축하해 ㅋㅋㅋ 담에 함 보자! 오늘 하루 즐겁게 잘보내구',
+    },
+  ];
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <CommentInput />
-      <CommentList />
+      <CommentList commentData={commentData} />
     </div>
   );
 };
