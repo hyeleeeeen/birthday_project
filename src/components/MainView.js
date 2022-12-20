9dee413fbccde88e06fa7c600243b7839839df0e
hyeleeeeen <heo22969087@gmail.com>
2022-12-20 18:29:42 +0900

    :art: style: 이미지추가, css 수정

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,10 +1,16 @@
 import mainpic from '../asset/images/mainpic.jpeg';
+import luv from '../asset/images/luv.jpg';
 
 const MainView = () => {
   return (
     <>
       <main className="flex flex-col font-bold text-2xl text-gray-500  ">
-        <img src={mainpic} alt="pic" className="w-1/2  m-auto" />
+        <img
+          src={mainpic}
+          alt="pic"
+          className="w-1/2  m-auto border-4 border-dashed border-4 border-dashed"
+        />
+        <img className="w-1/2  m-auto mt-10" src={luv} alt="pic"></img>
       </main>
     </>
   );
