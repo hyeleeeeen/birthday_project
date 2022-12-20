c8abf988152a409cf99a28f70ee62289c25efef2
hyeleeeeen <heo22969087@gmail.com>
2022-12-20 18:32:03 +0900

    :recycle:refactor: 태그 수정

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,18 +1,18 @@
 const CommentList = ({ commentData }) => {
   return (
-    <section className="bg-[#FAF8ED] p-10 rounded-[4rem]">
+    <ul className="bg-[#FAF8ED] border-4 border-dashed border-[#d9d9d9] p-10 rounded-[4rem]">
       {commentData.map((item) => {
         return (
-          <div
+          <li
             key={item.id}
-            className="flex flex-col w-4/5 m-auto mt-4 border border-[#D9D9D9] bg-white rounded-full"
+            className="flex flex-col w-4/5 m-auto mt-4 border  border-[#D9D9D9] bg-white rounded-full"
           >
-            <span className="text-left ml-4 mt-2">{item.content}</span>
-            <span className="text-right mr-4 mb-2">{item.nickName}</span>
-          </div>
+            <span className="text-left ml-4 mt-2"> {item.content}</span>
+            <span className="text-right mr-4 mb-2">from {item.nickName}</span>
+          </li>
         );
       })}
-    </section>
+    </ul>
   );
 };
 
