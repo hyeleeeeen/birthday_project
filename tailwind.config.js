423ad49a7ad09c2fb1f56532278667c2daf57205
hyeleeeeen <heo22969087@gmail.com>
2022-12-20 18:28:24 +0900

    :art:style : 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.js', './public/index.html'],
   theme: {
-    extend: {},
+    extend: { fontFamily: { sans: ['Nanum', 'Arial', 'sans-serif'] } },
   },
   plugins: [],
 };
