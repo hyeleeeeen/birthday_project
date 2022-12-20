90c5decc3b108fa29e72fb08f2de592bb2fcf942
hyeleeeeen <heo22969087@gmail.com>
2022-12-20 18:33:07 +0900

    :art: style: 이미지 추가

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,5 +1,11 @@
+import collage from '../asset/images/collage2.png';
+
 const Gallery = () => {
-  return;
+  return (
+    <main className="flex flex-col justify-center border-4 border-dashed border-4 border-dashed">
+      <img className="w-full " src={collage} alt="we"></img>
+    </main>
+  );
 };
 
 export default Gallery;
