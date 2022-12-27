2eb02869fc8c3bb45b8612cb2bd91671fad4482a
hyeleeeeen <heo22969087@gmail.com>
2022-12-27 17:13:38 +0900

    :art:style: 줄넘침 방지 코드 추가

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -5,7 +5,7 @@ const CommentList = ({ commentData }) => {
         return (
           <li
             key={item.id}
-            className="flex flex-col w-4/5 m-auto mt-4 border  border-[#D9D9D9] bg-white rounded-full max-md:h-32"
+            className="flex flex-col w-4/5 m-auto mt-4 border  border-[#D9D9D9] bg-white rounded-full max-md:h-32 break-all"
           >
             <span className="text-left ml-4 mt-8 h-10 "> {item.content}</span>
             <span className="text-right  text-sm text-gray-400 max-md:mt-4 mr-10">
