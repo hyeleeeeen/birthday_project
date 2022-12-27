3a0ec688bb70941764cf615b079f412e6daa99b2
hyeleeeeen <heo22969087@gmail.com>
2022-12-27 15:42:28 +0900

    :sparkle: feat: 하단 페이지 이동 기능 추가

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,18 +1,20 @@
 import mainpic from '../asset/images/mainpic.jpeg';
 import luv from '../asset/images/luv.jpg';
+import { GoChevronRight } from 'react-icons/go';
 
-const MainView = () => {
+const MainView = ({ Link }) => {
   return (
-    <>
-      <main className="flex flex-col font-bold text-2xl text-gray-500  ">
-        <img
-          src={mainpic}
-          alt="pic"
-          className=" m-auto border-4 border-dashed w-10/12"
-        />
-        <img className=" m-auto mt-10 w-10/12" src={luv} alt="pic"></img>
-      </main>
-    </>
+    <main className="flex flex-col font-bold text-2xl text-gray-500  ">
+      <img
+        src={mainpic}
+        alt="pic"
+        className=" m-auto border-4 border-dashed w-10/12"
+      />
+      <img className=" m-auto mt-10 w-10/12" src={luv} alt="pic"></img>
+      <Link to="/happy" className="flex flex-row-reverse">
+        <GoChevronRight className="text-6xl sm:text-8xl text-gray-400" />
+      </Link>
+    </main>
   );
 };
 
